d18ff4bfb4b01445c61058e590ca93cd06afabec
M. Saad Iqbal <114656892+Saad-Iqbal-Awan@users.noreply.github.com>
2024-05-06 08:27:43 +0200

    get the grid to generate with proper height and width dynamically.

--- main.js
+++ main.js
@@ -1,10 +1,30 @@
 console.log("it works")
 
-const first = document.querySelector("#first");
-const second = document.querySelector('#second');
+const container = document.querySelector("#container");
 
-first.addEventListener("mouseenter", change);
 
-function change() {
-    first.style.backgroundColor = 'yellow';
+let width = 400;
+let height = 400;
+let num = prompt('size of grid?')
+let grid = num * num
+
+for (let i = 0; i < grid; i++) {
+    newDiv = document.createElement('div');
+    newDiv.classList.add('box');
+    newDiv.style.width = width/num + 'px';
+    newDiv.style.height = height/num + 'px';
+    container.appendChild(newDiv);
+    // newDiv.addEventListener("mouseenter", change);
+    
 }
+
+// get node list of all boxes
+let boxes = document.querySelectorAll(".box"); 
+
+// add event listener to all boxes
+boxes.forEach(function(box) {
+    box.addEventListener("mouseenter", function() {
+        box.style.backgroundColor = 'yellow';
+    });
+    console.log("it now works");
+})
